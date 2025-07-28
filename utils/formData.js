e9182b8284d1ec498de408c1af7a5d79a4814fa0
Shweta93J <165642375+Shweta93J@users.noreply.github.com>
2025-07-28 08:30:46 +0400

    Create formData.js

---
+++ utils/formData.js
@@ -0,0 +1,14 @@
+module.exports = {
+  validUser: {
+    firstName: 'Shweta',
+    lastName: 'Kumari',
+    email: 'shweta.k@example.com',
+    phone: '971501234567'
+  },
+  invalidEmail: {
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'invalid-email',
+    phone: '971501234567'
+  }
+};
